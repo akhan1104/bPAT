f058bd8ce6e362cdc11c33c8c5616d7adf55f062
Aleem Khan <AleemKhan@Aleems-MBP-2.home>
2017-12-25 16:58:42 -0500

    Working code to extract an overall summary, batter/pitcher box score, and event summary from gameday xml data

--- gamedayxml.py
+++ gamedayxml.py
@@ -1,6 +1,7 @@
 # functions to parse mlbgameday xml data
 # Created: 12/14/17
 from urllib2 import urlopen
+from datetime import date, timedelta
 import xml.etree.ElementTree as ET
 import pandas as pd
 
@@ -12,14 +13,65 @@ def getGameIDs(url):
     f.close()
 
     root = ET.fromstring(data)
-
+    
+    
     base = 'http://gd2.mlb.com'
     
-    return [base + c.attrib['game_data_directory'] for c in root]
+    ret = []
+    for c in root:
+        # remove PPD games
+        a = c.find('status')
+        if a.attrib['status'] != 'Postponed':
+            ret.append(base + c.attrib['game_data_directory'])
+
+    return ret
    
 
+def parseEvents(url):
+    """ Given a gameday url parse out events from game_events.xml
+        Input: Single base gameday URL as a string
+        Output: Single DataFrame with event info """
+
+    f = urlopen(url + '/game_events.xml')
+    data = f.read()
+    f.close()
+
+    root = ET.fromstring(data)
+    game_id = url[url.find('gid') : url.find('/box')]
+
+    gameEvents = pd.DataFrame()
+
+    # traverse tree pull out desired info
+        # gameid, ab_num, b, s, o, batter, pitcher, event_num, event, b1(baserunner1)
+        # b2, b3, final pitch type, final pitch speed 
+    for inning in root.findall('inning'):
+        for half in list(inning):
+            for ab in half.findall('atbat'):
+                #some xml errors found on mlb side
+                try:
+                    info = ab.attrib
+                    last_pitch = ab.findall('pitch')[-1]
+                except IndexError:
+                    continue
+               
+                d = {'game_id' : game_id, 'ab_num' : info['num'],
+                     'b' : info['b'], 's' : info['s'], 'o' : info['o'],
+                     'batter' : info['batter'], 'pitcher' : info['pitcher'] ,
+                     'event_num' : info['event_num'] , 'event' : info['event'],
+                     'b1' : info['b1'], 'b2' : info['b2'], 'b3' : info['b3'],
+                     'pitch_type' : last_pitch.attrib['pitch_type'],
+                     'pitch_speed' : last_pitch.attrib['start_speed']}
+                gameEvents = pd.concat([gameEvents, pd.DataFrame(d, index=[int(info['num'])])])
+                
+    return gameEvents
+
+
+
 def parseBoxScore(url):
-    """ Given a gameday url parse out desired stats from boxscore.xml"""
+    """ Given a gameday url parse out desired stats from boxscore.xml
+        Input: Single base gameday URL as a string
+        Output: Tuple of size 3, None if game wasn't played, 3 data frames if it was"""
+
     f = urlopen(url + '/boxscore.xml')
     data = f.read()
     f.close()
@@ -27,6 +79,10 @@ def parseBoxScore(url):
 
     root = ET.fromstring(data)
     
+    # If game was ppd or not final, throw it out
+    if root.attrib['status_ind'] != 'F':
+        return None, None, None
+
     # store linescores as a string
     linescore = root.find('linescore')
     innings = list(linescore)
@@ -37,6 +93,7 @@ def parseBoxScore(url):
     # store overall game summary as dataframe
     game_summary = pd.DataFrame(root.attrib, index=[0])
     game_summary['game_id'].loc[0] = game_id
+    game_summary['linecore'] = LS
     
     # go through tags and find batting/pitching stats
     batting, pitching = list(), list()
@@ -61,30 +118,65 @@ def parseBoxScore(url):
 
 
 def urlCombiner(year, month, day):
-    """ Returns a base url for a given year, month, date combination"""
+    """ Returns a base url for a given year, month, date combination
+        Input: Three values (string or int) as year, month, day
+        Output: A string representing the URL of a gameday directory """
 
     base_url = 'http://gd2.mlb.com'
+    # directories require 0 in front of single digits
+    if int(month) < 10:
+        month = '0' + str(month)
+    if int(day) < 10:
+        day = '0' + str(day)
+
     return base_url + '/components/game/mlb/' + 'year_' + str(year) + '/month_' + str(month) + '/day_' + str(day)
 
+
+def getSeasonDays(start, end):
+    """ Get a list of gameday directories given an inseason date range """
+    delta = end - start
+    urls = []
+    for i in range(delta.days + 1):
+        currDay = start+ timedelta(days=i)
+        urls.append(urlCombiner(currDay.year, currDay.month, currDay.day))
+    return urls
+
+
+### Test Script ### - Eventually just functions
 year = 2017
 month = '08'
-day = 21
+day = 11
 
+time = (year, month, day)
+t0_2017 = date(2017, 4, 02)
+tf_2017 = date(2017, 5, 01)
+tDelta = tf_2017 - t0_2017
 
+#get all days in a season as a list of URL directories
+all_days = getSeasonDays(t0_2017, tf_2017)
+
+#get a list of lists representing all gameday directories
+all_games = [getGameIDs(i) for i in all_days]
+
+#flatten list
+flat_all_games = [item for sublist in all_games for item in sublist]
 
 games_url = urlCombiner(year, month, day)
-#get all games for a single day
-all_games = getGameIDs(games_url)
 
 #get all stats for a single day
 
-summary, hitters, pitchers = pd.DataFrame(), pd.DataFrame(), pd.DataFrame()
+summary, hitters, pitchers, events = pd.DataFrame(), pd.DataFrame(), pd.DataFrame(), pd.DataFrame()
 
-for i in all_games:
+for i in flat_all_games:
+    print i
     s, h, p = parseBoxScore(i)
+    ev = parseEvents(i)
     summary = pd.concat([summary, s])
-    hitters = pd.concat([hitters, b])
+    hitters = pd.concat([hitters, h])
     pitchers = pd.concat([pitchers, p])
+    events = pd.concat([events, ev])
+    events.reset_index(inplace=True)
+
 
 
 
